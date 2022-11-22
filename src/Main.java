b5cc1b10b90e65c211469067f7ae58251bf8dd89
Dharani Gowtham <dharaniadithya1998.da@gmail.com>
2022-11-22 16:54:11 +0530

    Main Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // All main programs are present with their package itself
     }
 }
